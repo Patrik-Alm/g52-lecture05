95f6c26afff83b2a0d95586649a93f4e554b743c
Patrik Alm <almpatrik@yahoo.se>
2024-10-14 16:08:36 +0200

    Oil and Water

--- src/main/java/io/github/patrikalm/Main.java
+++ src/main/java/io/github/patrikalm/Main.java
@@ -1,7 +1,29 @@
 package io.github.patrikalm;
 
+
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        String sentence = "Oil and Water";
+
+        String oil = sentence.substring(0, 4);
+
+        String water = sentence.substring(8);
+
+        String[] array = {oil, water};
+
+        System.out.println(oil);
+
+        System.out.println(water);
+
+        for (String s : array) {
+            System.out.println(s);
+        }
+
+
+
+
     }
 }
