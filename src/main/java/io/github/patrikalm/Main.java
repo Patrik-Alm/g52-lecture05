ef716a4a4ee56d70f2e98ecbd31c95b0d42a94de
Patrik Alm <almpatrik@yahoo.se>
2024-10-15 10:01:19 +0200

    CharArray to String variable

--- src/main/java/io/github/patrikalm/Main.java
+++ src/main/java/io/github/patrikalm/Main.java
@@ -4,18 +4,27 @@ package io.github.patrikalm;
 public class Main {
     public static void main(String[] args) {
 
-        String line = "ThisShouldBeConverted";
+       /* String line = "ThisShouldBeConverted";
 
         char[] charArray = line.toCharArray();
 
         for (char i : charArray) {
             System.out.println(i);
-        }
+        } */
 
+        char[] myArray = new char[]{'J', 'a', 'v', 'a'};
+        String myLine = "";
 
+        System.out.print(myArray);
+        System.out.println(" from Char Array");
 
+        for(char i : myArray) {
 
+            myLine = myLine + i;
 
+            }
+
+        System.out.println(myLine + " from String variable");
 
     }
 }
